afdd2cc53be1c088f797a79645b990c657c3097b
Cheolkie Kim <ckiekim@naver.com>
2022-10-25 17:34:45 +0900

    Chapter 4. 조건문과 반복문

--- src/ch04/Ex05_FizzBuzz.java
+++ src/ch04/Ex05_FizzBuzz.java
@@ -1,6 +1,7 @@
 package ch04;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /*
  * FizzBuzz 문제
@@ -14,10 +15,16 @@ public class Ex05_FizzBuzz {
 
 	public static void main(String[] args) {
 		// 임의의 정수값을 생성
-		Random ran = new Random();
+		Random ran = new Random();		// 객체 생성
 		int number = ran.nextInt(100) + 1;	// 1 ~ 100 까지 임의의 정수값 생성
 		
-		if (number % 15 == 0)
+		System.out.print("1에서 100까지의 정수를 입력하세요.> ");
+		Scanner scan = new Scanner(System.in);		// 객체 생성
+		int num = scan.nextInt();
+		number = num;
+		scan.close();
+		
+		if (number % 15 == 0)		// if (number % 3 == 0 && number % 5 == 0)
 			System.out.println(number + ", FizzBuzz");
 		else if (number % 3 == 0)
 			System.out.println(number + ", Fizz");
