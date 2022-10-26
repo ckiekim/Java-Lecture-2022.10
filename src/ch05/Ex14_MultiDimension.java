93e41ff4a5167010e3ff243bced5f069303755b2
Cheolkie Kim <ckiekim@naver.com>
2022-10-26 15:50:55 +0900

    Chapter 5. 참조 타입

---
+++ src/ch05/Ex14_MultiDimension.java
@@ -0,0 +1,14 @@
+package ch05;
+
+/**
+ * 다차원 배열
+ */
+public class Ex14_MultiDimension {
+
+	public static void main(String[] args) {
+		int[][] matrix = new int[2][3];		// 행의 갯수가 2, 열의 갯수가 3
+		int[][] score = { {80, 90, 84}, {78, 85, 95} };		// 학생 2명의 국어/영어/수학 성적
+		
+	}
+
+}
