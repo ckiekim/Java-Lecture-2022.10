0e3de8cfc314f0153f422fb1aade2a0a83291758
Cheolkie Kim <ckiekim@naver.com>
2022-11-18 10:47:47 +0900

    MySQL customer

--- src/mysql/customer/Main.java
+++ src/mysql/customer/Main.java
@@ -14,12 +14,17 @@ public class Main {
 //		Customer c2 = new Customer("james","제임스");
 //		dao.insertCustomer(c2);
 		
-//		List<Customer> list = dao.getCustomers();
-//		for (Customer c: list)
-//			System.out.println(c);
-		
 		Customer c3 = dao.getCustomer("james");
 		System.out.println(c3);
+		System.out.println();
+		
+		c3.setName("재임수");
+		c3.setRegDate(LocalDate.parse("2022-11-01"));
+		dao.updateCustomer(c3);
+		
+		List<Customer> list = dao.getCustomers();
+		for (Customer c: list)
+			System.out.println(c);
 	}
 
 }
