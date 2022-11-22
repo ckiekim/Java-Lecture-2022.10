9cfcae83290a2e5561de2adab0c637fd85ed03c6
Cheolkie Kim <ckiekim@naver.com>
2022-11-22 14:31:01 +0900

    MySQL Shopping

--- src/mysql/bbs/UsersTest.java
+++ src/mysql/bbs/UsersTest.java
@@ -10,22 +10,22 @@ public class UsersTest {
 
 	public static void main(String[] args) {
 		
-//		dao.registerUser(new Users("admin","admin","관리자","admin@mysql.com"));
-//		dao.registerUser(new Users("james","james","제임스","james@mysql.com"));
+		dao.registerUser(new Users("maria","maria","마리아","maria@mysql.com"));
+		dao.registerUser(new Users("kevin","kevin","케빈","kevin@mysql.com"));
 
 //		Users user = dao.getUserInfo("admin");
 //		System.out.println(user);
 		
-//		List<Users> list = dao.listUsers();
-//		for (Users u: list)
-//			System.out.println(u);
+		List<Users> list = dao.listUsers();
+		for (Users u: list)
+			System.out.println(u);
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.print("uid> ");
-		String uid = scan.nextLine();
-		System.out.print("pwd> ");
-		String pwd = scan.nextLine();
-		login(uid, pwd);
+//		Scanner scan = new Scanner(System.in);
+//		System.out.print("uid> ");
+//		String uid = scan.nextLine();
+//		System.out.print("pwd> ");
+//		String pwd = scan.nextLine();
+//		login(uid, pwd);
 	}
 	
 	static void login(String uid, String pwd) {
